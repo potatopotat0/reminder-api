a452a977757b84e87035cfa00b2d03a1f0bfb7da
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-09-17 18:54:48 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import bunyan from 'bunyan'
+import crypto from 'crypto'
+import upath from 'upath';
+import fs from 'fs';
+const app = express();
+const logger = bunyan.createLogger({
+    name: "reminder-api",
+    level: bunyan.INFO,
+    stream: process.stdout
+});
+const imgFolder = 'img_assets/';
+const imgName = fs.readdirSync(upath.join(__dirname, imgFolder));
+function getRandomElementFromArray(arr: Array<any>) {
+    return arr[crypto.randomInt(arr.length)]
+}
+
+
+logger.info("Loaded image list:");
+logger.info(imgName);
+
+app.get("/random", (req, res) => {
+    res.type("text/plain; charset=utf-8");
+    res.end(getRandomElementFromArray(imgName));
+})
+
+app.get("/image", (req, res) => {
+    if (typeof req.query.img == "string" && imgName.includes(req.query.img)) {
+        res.sendFile(upath.join(__dirname, imgFolder, req.query.img));
+    } else {
+        res.status(404);
+        res.end();
+    }
+})
+
+let port = 9999;
+if (process.env.PORT && !isNaN(parseInt(process.env.PORT))) {
+    port = parseInt(process.env.PORT);
+}
+
+app.listen(port);
+logger.info(`Server starts listening on ${port}`);
